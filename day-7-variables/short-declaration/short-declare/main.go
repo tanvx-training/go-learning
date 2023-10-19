a719ba9cdf0cfa36f608dc9985fcec5d955ab774
TanVX <tanvx.training@gmail.com>
2023-10-20 00:10:05 +0700

    day-7

---
+++ day-7-variables/short-declaration/short-declare/main.go
@@ -0,0 +1,35 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+import "fmt"
+
+// ---------------------------------------------------------
+// EXERCISE: Short Declare
+//
+//  Declare and then print four variables using
+//  the short declaration statement.
+//
+// EXPECTED OUTPUT
+//  i: 314 f: 3.14 s: Hello b: true
+// ---------------------------------------------------------
+
+func main() {
+	// ADD YOUR DECLARATIONS HERE
+	//
+	i, f, s, b := 314, 3.14, "Hello", true
+	// THEN UNCOMMENT THE CODE BELOW
+
+	fmt.Println(
+		"i:", i,
+		"f:", f,
+		"s:", s,
+		"b:", b,
+	)
+}
---
+++ day-7-variables/short-declaration/short-with-expression/main.go
@@ -0,0 +1,31 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+import "fmt"
+
+// ---------------------------------------------------------
+// EXERCISE: Short With Expression
+//
+// 	1. Short declare a variable named `sum`
+//
+//  2. Initialize it with an expression by adding 27 and 3.5
+//
+// EXPECTED OUTPUT
+//  30.5
+// ---------------------------------------------------------
+
+func main() {
+	// ADD YOUR DECLARATION HERE
+	//
+	var sum float64
+	sum = 27 + 3.5
+	// THEN UNCOMMENT THE CODE BELOW
+	fmt.Println(sum)
+}
